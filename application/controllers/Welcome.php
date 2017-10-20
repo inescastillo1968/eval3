3bc718a36753c5589ddc05c21b68bfdb96196abc
inescastillo1968 <inesmayo12345@hotmail.com>
2017-10-19 21:53:44 -0300

    crear funcion
    para recibir datos en el controlador

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -4,47 +4,42 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Welcome extends CI_Controller {
 
 	function __construct(){
+
+
+
 		parent::__construct();
-		$this->load->database();//esta linea siempre debe ir 
-	}
-
-
-
-	//esta funcion es la que guarda en la base de dato
-	function guardar($data)
-	{
-		$query = $this->db->insert('usuarios', array('nombre'=>$data['nombre'], 'apellido'=>$data['apellido']));
-
-		if ($this->db->affected_rows() > 0) 
-		{
 		$this->load->helper('form');
-
-		//$this->load->model('form_model');  
+		$this->load->model('form_model'); 
 	}
-
-	public function index()
+		public function index()
 	{
 		$this->load->view('formulario');
 	}
+	
+
+	//esta funcion es la que guarda en la base de dato
+	
+
+	
 
 	function validar()
 // echo aqui se hacen los cambios
 
 {
 
-$name = $this->input->post('nombre2');
-$lastname = $this->input->post('apellido_paterno');
+$name = $this->input->post('nombre');
+$lastname = $this->input->post('apellido');
 $correo = $this->input->post('correo');
-echo "nombre:".$nombre2."apellido_paterno:".$lastname."correo:".$"correo";
-		
-	//estamos creando array		
-			
-		
+echo "nombre:".$name."apellido:".$lastname."correo:".$"email";
 
-		$data = array('nombre' => $this->input->post('nombre2' ),'apellido' => $this->input->post('apellido_paterno') );
-		//aqui se llama la funcion para guardar en la base de datos
+		
+	
+$data = array('nombre' => $this->input->post('nombre' ),'apellido' => $this->input->post('apellido')"correo"=>$this=>input->post("correo"));
+		
 		$this->form_model->guardar($data);
 	}
+
+
 
 }
 			
